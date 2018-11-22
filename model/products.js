2703e92e17360a838666df1f1801f6056de0f244
Евгений Войтенко <e.voitenko@mangotele.com>
2018-11-22 15:49:04 +0300

    Release

---
+++ model/products.js
@@ -0,0 +1,11 @@
+module.exports = Object.freeze({
+    SIP = 'SIP',
+    LK = 'LK',
+    CC = 'CC',
+    DKT = 'DKT',
+    API = 'API',
+    MTALKER = 'MTALKER',
+    KONS = 'KONS',
+    OTHER = 'OTHER',
+    EMPTY = 'EMPTY'
+});
